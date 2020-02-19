ffdd8d5256cb596a55014f90536eb6b06dd5870a
Jacob <jacobrpyoung@gmail.com>
2020-02-19 15:19:28 -0500

    Config argument

--- analysis.py
+++ analysis.py
@@ -1,4 +1,8 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='View data collected from Scouting App')
-parser.add_argument()
+parser.add_argument('--config', '-c', required=True, help='Config filename')
+
+args = parser.parse_args()
+
+print(args.config)
