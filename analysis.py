9ff08b4488c8c6caa6159505cd143eac22209b58
Jacob <jacobrpyoung@gmail.com>
2020-02-19 15:49:29 -0500

    arguments

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='View data collected from Scouting App')
-parser.add_argument('--config', '-c', required=True, help='Config filename')
+parser.add_argument('--database', '-d', required=True, help='Choose database')
 parser.add_argument('--teams', '-t', nargs='+', required=True, help='')
 parser.add_argument('--x_axis', '-x', nargs='+', required=True, help='')
 parser.add_argument('--y_axis', '-y', nargs='+', required=True, help='')
