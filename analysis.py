82f68f0894f02b080fca1bf5fd2685456d7cc4f7
Brendon Matusch <brendon-m@outlook.com>
2020-02-20 14:25:44 -0500

    Create histogram in addition to line plot

--- analysis.py
+++ analysis.py
@@ -10,8 +10,8 @@ from firebase_admin import firestore
 # Arguments for output graph
 parser = argparse.ArgumentParser(description='View data collected from Scouting App')
 parser.add_argument('--teams', '-t', nargs='+', required=True, help='Enter in team numbers to be displayed')
-parser.add_argument('--x_axis', '-x', nargs='+', required=True, help='Values displayed on x-axis')
-parser.add_argument('--y_axis', '-y', nargs='+', required=True, help='Values displayed on y-axis')
+parser.add_argument('--x_axis', '-x', required=True, help='Values displayed on x-axis')
+parser.add_argument('--y_axis', '-y', required=True, help='Values displayed on y-axis')
 parser.add_argument('--cred', '-c', required=True, help='Path to database credentials')
 
 args = parser.parse_args()
@@ -22,12 +22,9 @@ cred = firebase_admin.credentials.Certificate(args.cred)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-# Graph stuff
-current_axis = plt.gca()
-
 # Blank lists for values collected for graphing
-x = [[[] for i in range(len(args.x_axis))] for i in range(len(args.teams))]
-y = [[[] for i in range(len(args.y_axis))] for i in range(len(args.teams))]
+x = [[] for i in range(len(args.teams))]
+y = [[] for i in range(len(args.teams))]
 
 # Make sure we find all the teams; if not, exit with an error
 found_teams = {num: False for num in args.teams}
@@ -43,17 +40,16 @@ for response in db.collection('response').stream():
     # Only save data to relevant teams
     if response_dict['Team Number'] in args.teams:
         # Do this for the x and y axis values
-        for axis_args, array in [(args.x_axis, x), (args.y_axis, y)]:
-            for i, attr in enumerate(axis_args):
-                team_number = response_dict['Team Number']
-                found_teams[team_number] = True
-                team_idx = args.teams.index(team_number)
-                # Sometimes there are blank strings
-                try:
-                    attr_value = float(response_dict[attr])
-                except ValueError:
-                    attr_value = np.nan
-                array[team_idx][i].append(attr_value)
+        for attr, array in [(args.x_axis, x), (args.y_axis, y)]:
+            team_number = response_dict['Team Number']
+            found_teams[team_number] = True
+            team_idx = args.teams.index(team_number)
+            # Sometimes there are blank strings
+            try:
+                attr_value = float(response_dict[attr])
+            except ValueError:
+                attr_value = np.nan
+            array[team_idx].append(attr_value)
 
 teams_not_found = []
 for num in args.teams:
@@ -63,19 +59,28 @@ if teams_not_found:
     print('Teams', teams_not_found, 'not found')
     sys.exit()
 
+fig, (line_ax, hist_ax) = plt.subplots(1, 2)
+
 for team in range(len(args.teams)):
-    for i in range(len(args.y_axis)):
-        color = next(current_axis._get_lines.prop_cycler)['color']
-        # Sort the X axis values so that the line graph looks right
-        sort_order = np.argsort(x[team][0])
-        x_values = np.array(x[team][0])[sort_order]
-        y_values = np.array(y[team][i])[sort_order]
-        plt.plot(x_values, y_values, label=args.teams[team], color=color)
+    color = next(line_ax._get_lines.prop_cycler)['color']
 
-        # Draw a secondary line containing the mean for that team
-        mean = np.nanmean(y_values)
-        x_range = [np.nanmin(x_values), np.nanmax(x_values)]
-        plt.plot(x_range, [mean, mean], label=args.teams[team]+' Mean', linestyle='--', color=color)
+    # Sort the X axis values so that the line graph looks right
+    sort_order = np.argsort(x[team])
+    x_values = np.array(x[team])[sort_order]
+    y_values = np.array(y[team])[sort_order]
+    line_ax.plot(x_values, y_values, label=args.teams[team], color=color)
 
-plt.legend()
+    # Draw a secondary line containing the mean for that team
+    mean = np.nanmean(y_values)
+    x_range = [np.nanmin(x_values), np.nanmax(x_values)]
+    line_ax.plot(x_range, [mean, mean], label=args.teams[team]+' Mean', linestyle='--', color=color)
+
+    # Also create a histogram of the variable for this team
+    hist_ax.hist(y_values, color=color, histtype='stepfilled', alpha=0.2)
+
+line_ax.set_xlabel(args.x_axis)
+line_ax.set_ylabel(args.y_axis)
+hist_ax.set_xlabel(args.y_axis)
+hist_ax.set_ylabel('Occurrences')
+fig.legend()
 plt.show()
